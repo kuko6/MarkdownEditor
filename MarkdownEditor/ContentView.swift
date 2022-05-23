7c2b72fe8237f1103080c97856b090a91f713614
Jakub Povinec <jakub.povinec@gmail.com>
2022-05-23 11:29:46 +0200

    Initial Commit

---
+++ MarkdownEditor/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MarkdownEditor
+//
+//  Created by kuko on 23/05/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MarkdownEditor/MarkdownEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarkdownEditorApp.swift
+//  MarkdownEditor
+//
+//  Created by kuko on 23/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct MarkdownEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
