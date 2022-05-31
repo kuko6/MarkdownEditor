ecee1d05eca23035f05cc8dc6bc196ab32cee197
Jakub Povinec <jakub.povinec@gmail.com>
2022-05-31 21:59:24 +0200

    added code highlighting

--- MarkdownEditor/Views/WebView.swift
+++ MarkdownEditor/Views/WebView.swift
@@ -19,7 +19,15 @@ struct WebView: NSViewRepresentable {
             print("Could not load the css file!!")
         }
         
-        self.html = "<head><style>\(cssString)</style></head><body>\(html)</body>"
+        // TODO: - Change style to "github-dark" in dark mode and also change the background color for pre element in the css file
+        let highlightjsHead = """
+        <link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.5.1/styles/github.min.css\">
+        <script src=\"https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.5.1/highlight.min.js\"></script>
+        """
+        let highlightjsScript = "<script>hljs.initHighlightingOnLoad();</script>"
+
+        self.html = "<head><style>\(cssString)</style>\(highlightjsHead)</head><body>\(highlightjsScript)\(html)</body>"
+        // print(self.html)
     }
     
     func makeNSView(context: Context) -> WKWebView {
--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # MarkdownEditor
 A simple markdown editor made in Swift.
 
+For code syntax highlighting we used [highlight.js](https://highlightjs.org) included in the preview html. 
+
 ### Dependencies
 [Ink](https://github.com/JohnSundell/Ink) - markdown parser
